b19b567865dbff5476e7ef44e1dbe3a8027e7fcc
hienhayho <115549171+hienhayho@users.noreply.github.com>
2024-02-15 22:58:17 +0700

    add load custom dataset

--- datasets/load_dataset.py
+++ datasets/load_dataset.py
@@ -1,5 +1,6 @@
 from .mnist import load_mnist
 from .cifar import load_cifar
+from .custom import load_custom
 
 def load_dataset(args):
     if args.dataset == 'mnist':
@@ -7,7 +8,7 @@ def load_dataset(args):
     elif args.dataset == 'cifar':
         train_loader, test_loader = load_cifar(args)
     elif args.dataset == 'custom':
-        pass
+        train_loader, test_loader = load_custom(args)
     else:
         raise NotImplementedError
     return train_loader, test_loader
