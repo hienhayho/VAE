4e32ddd744543f8b9fc7b2c31aefdaaf4799a045
hienhayho <115549171+hienhayho@users.noreply.github.com>
2024-02-18 19:27:04 +0700

    add __init__

---
+++ assets/__init__.py
@@ -0,0 +1,3 @@
+from .date import get_current_date
+from .logging import configure_logging, initial_logging, log_info, finish_logging, log_error, log_warning, get_environment_info
+from .utils import init_result_path, parse_args, set_seed
